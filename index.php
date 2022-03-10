3b872e7ac05bf7c7f69ebad7cff01e67746239b0
angelmfb <91120967+angelmfb@users.noreply.github.com>
2022-03-10 01:09:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="amfernandezbanos.guadalupe@alumnado.fundacionloyola.net>">
+        <link rel="stylesheet" href="src/css/style.css">
+        <title>Repaso examen</title>
+    </head>
+    <body>
+        <header>
+            <h1>Repaso examen</h1>
+            <nav>
+                <ul>
+                    <li><a href="./src/views/v_registro.php">Registro Usuario</a></li>
+                    <li><a href="./src/views/v_iniciosesion.php">Inicio Sesión</a></li>
+                    <li><a href="./src/views/v_eliminar.php">Eliminar</a></li>
+                    <li><a href="./src/views/v_listar.php">Listar</a></li>
+                    <li><a href="./src/views/v_modificar.php">Modificar</a></li>
+                    <li><a href="./src/views/v_cerrarsesion.php">Cerrar Sesión</a></li>
+                    <li><a href="./src/html/subidaArchivos.html">Subir Archivos</a></li>
+                    <li><a href="./src/php/verFicheros.php">Ver directorio de subida de archivos</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <p>
+                Repaso para el examen.
+            </p>
+        </main>
+    </body>
+</html>
---
+++ sql/script.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE tienda_fotos_online;
+USE tienda_fotos_online;
+
+CREATE TABLE cliente(
+    idCliente tinyint UNSIGNED primary key AUTO_INCREMENT not null,
+    nombre varchar(50) not null,
+    password varchar(20) not null,
+    direccion varchar(60) not null
+);
+CREATE TABLE pedido(
+    idPedido smallint UNSIGNED primary key AUTO_INCREMENT not null,
+    fechaHora datetime not null,
+    idCliente tinyint UNSIGNED not null,
+    ruta varchar(50) null,
+    CONSTRAINT FK_idCliente FOREIGN KEY (idCliente) REFERENCES cliente(idCliente) ON DELETE CASCADE ON UPDATE CASCADE
+);
